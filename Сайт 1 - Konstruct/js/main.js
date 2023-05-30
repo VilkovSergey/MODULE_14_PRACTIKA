1fca60697ac1be735f0e286433abf53285455e95
VilkovSergey <vilkov75@yandex.ru>
2023-05-30 17:00:27 +0300

    End section - services and Start section - get

--- Сайт 1 - Konstruct/js/main.js
+++ Сайт 1 - Konstruct/js/main.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // var instances_sidenav = M.Sidenav.init(sidenav);
   var modal = document.querySelectorAll('.modal');
   var instances_modal = M.Modal.init(modal);
-  var gallery = document.querySelectorAll('.materialboxed');
-  var instances_gallery = M.Materialbox.init(gallery);
+  // var gallery = document.querySelectorAll('.materialboxed');
+  // var instances_gallery = M.Materialbox.init(gallery);
 });
 
 
